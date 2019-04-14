df770facda2d7b8d593a5f66e85395bd09853337
Gopikrishna Mandapati <mandapati.g.c@gmail.com>
2019-04-14 01:46:35 -0400

    Updates

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,12 @@
+
+public class HelloWorldTest {
+
+	public void testHello() {
+		System.out.println("Test - Hello ");
+	}
+
+	public void testWorld() {
+		System.out.println("Test -  World");
+	}
+
+}
